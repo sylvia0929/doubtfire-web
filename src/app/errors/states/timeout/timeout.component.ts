15ebfa4f3bd943f4ca6b1a0601cbcc06023d1dd2
sylvia0929 <529807748@qq.com>
2024-07-31 18:31:22 +1000

    refactor(timeout): migrate timeout component to Angular 17 and TypeScript

---
+++ src/app/errors/states/timeout/timeout.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input, Inject } from '@angular/core';
+
+@Component({
+  selector: 'timeout',
+  templateUrl: 'timeout.component.html',
+})
+export class TaskDescriptionCardComponent {
+  constructor() {}
+}
